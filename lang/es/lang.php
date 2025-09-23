ea80c71c8d330de4042ca58047cae9cec00b6a97
smocap <smocap@gmail.com>
2025-09-23 23:45:46 +0200

    translation update

--- lang/es/lang.php
+++ lang/es/lang.php
@@ -3,10 +3,40 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author smocap <smocap@gmail.com>
  * @author Yllelder <yllelder@gmail.com>
  * @author Joel Cantoral <cantoral.joel@gmail.com>
  * @author Domingo Redal <docxml@gmail.com>
  */
 $lang['pages']                 = 'Páginas de la galería:';
 $lang['js']['addgal']          = 'Añadir espacio de nombres como galería';
+$lang['js']['label_toolbar_button'] = 'Insertar galería';
+$lang['js']['title_dialog']    = 'Complemento Gallery';
+$lang['js']['label_namespace'] = 'Espacio de nombres o fuente RSS';
+$lang['js']['label_thumbnailsize'] = 'Establece el tamaño de las miniaturas';
+$lang['js']['pattern_hint_thumbnailsize'] = '<número><x minúscula><número>';
+$lang['js']['label_imagesize'] = 'Establece el tamaño de las imágenes enlazadas en modo directo.';
+$lang['js']['pattern_hint_imagesize'] = '<número><X mayúscula><número>';
+$lang['js']['label_cache']     = 'Almacenar en caché las miniaturas';
+$lang['js']['label_filter']    = 'Filtro (debe contener un carácter "*")';
+$lang['js']['label_showname']  = 'Mostrar el nombre del archivo debajo de las miniaturas';
+$lang['js']['label_showtitle'] = 'Mostrar la etiqueta EXIF "Titular" como el título debajo de las miniaturas';
+$lang['js']['label_crop']      = 'Hacer que las miniaturas tengan el tamaño exacto especificado, recortándolas cuando sea necesario';
+$lang['js']['label_direct']    = 'Enlazar las miniaturas con las imágenes de mayor tamaño, no con su página de detalles';
+$lang['js']['label_lightbox']  = 'Mostrar las imágenes en un modal elegante con JavaScript';
+$lang['js']['label_reverse']   = 'Invertir el orden de las imágenes mostradas';
+$lang['js']['label_recursive'] = 'Buscar imágenes en el espacio de nombres indicado y en todos sus subespacios de nombres';
+$lang['js']['label_align_left'] = 'alinear a la izquierda';
+$lang['js']['label_align_center'] = 'alinear al centro';
+$lang['js']['label_align_right'] = 'alinear a la derecha';
+$lang['js']['label_cols']      = 'Número de imágenes por fila en la tabla de galería';
+$lang['js']['label_limit']     = 'Limita la salida al número de imágenes indicado. 0 significa todas';
+$lang['js']['label_offset']    = 'Omitir las primeras imágenes. Útil con la opción anterior';
+$lang['js']['label_paginate']  = 'Añade una paginación para las miniaturas, mostrando la cantidad indicada de miniaturas por página. 0 desactiva la paginación';
+$lang['js']['label_sort_file'] = 'Ordenar las imágenes por nombre de archivo';
+$lang['js']['label_sort_random'] = 'Ordenar imágenes aleatoriamente. También podrías desactivar la caché';
+$lang['js']['label_sort_mod']  = 'Ordenar imágenes por fecha de modificación del archivo';
+$lang['js']['label_sort_exif_date'] = 'Ordenar imágenes por fecha EXIF';
+$lang['js']['label_sort_exif_title'] = 'Ordenar imágenes por título EXIF';
 $lang['nothingfound']          = 'No se han encontrado imágenes.';
+$lang['fail']                  = 'Error al cargar la galería';
